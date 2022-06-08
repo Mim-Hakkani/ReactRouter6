15b0fc95576af8c24ddcb7e2828d5ff73eff5d83
32-Mimhakkani <mim.cse32@gmail.com>
2022-06-08 10:14:21 +0600

    add some routes and route with element after click go to the specifig pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { ContextValue } from './ContextApi/Context/Context';
 import Main from './ContextApi/Main';
 import Nav from './Navigation/Nav';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Gallary from './pages/Gallary';
+import Home from './pages/Home';
 
 const datas =[
   {
@@ -33,6 +37,13 @@ function App() {
      
      <BrowserRouter>
       <Nav/>
+      <Routes>
+        <Route path='/' />
+        <Route path='/home'        element={<Home/>} />
+        <Route path='/about'   element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path="/gallary" element={<Gallary /> }/>
+      </Routes>
      </BrowserRouter>
 
      
