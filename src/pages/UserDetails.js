ae5cb087df98227516534502b5a4c1ef03c5fb31
32-Mimhakkani <mim.cse32@gmail.com>
2022-06-08 12:47:08 +0600

    useParams for dynamic id get

--- src/pages/UserDetails.js
+++ src/pages/UserDetails.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const UserDetails = () => {
-    const id = useParams();
-    console.log('amarid:',id.userId);
+    const {userId} = useParams();
+    // console.log('amarid:',id.userId);
     return (
         <div>
-            {/* DynamicRouting : {userId} */}
-            Name :'Mim',
-            Designation:Designer,
+            DynamicRouting : {userId} <br/>
+            Name :'Mim',<br/>
+            Designation:Designer,<br/>
             Roll :'React developer'
         </div>
     );
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -4,10 +4,8 @@ import { Outlet } from 'react-router-dom';
 const Users = () => {
     return (
         <div>
-            <h4>User profile 1</h4>
-            <h4>User profile 2</h4>
-            <h4>User profile 3</h4>
-            <h4>User profile 4</h4>
+            <h4>User profil</h4>
+           
             <Outlet/>
         </div>
     );
